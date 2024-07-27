4d2e711d25f911e8ac6c8cbce55d8e864b789328
naresy <nareshupadhayay74@gmail.com>
2024-07-26 23:24:16 -0700

    solution

--- Easy/find_followers.sql
+++ Easy/find_followers.sql
@@ -22,3 +22,6 @@
 -- The followers of 0 are {1}
 -- The followers of 1 are {0}
 -- The followers of 2 are {0,1}
+select user_id , count(follower_id)as followers_count from followers group by user_id
+order by 
+user_id asc
