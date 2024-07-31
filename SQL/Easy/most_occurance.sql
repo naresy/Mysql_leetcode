81e811ddbe49c037fe8727708ed236ed7115e067
naresy <nareshupadhayay74@gmail.com>
2024-07-30 22:47:11 -0700

    solution

--- SQL/Easy/most_occurance.sql
+++ SQL/Easy/most_occurance.sql
@@ -39,4 +39,18 @@
 -- +-----------------+
 -- | 1               |
 -- +-----------------+
--- Explanation: 1 is the only number that appears consecutively for at least three times.
+-- Explanation: 1 is the only number that appears consecutively for at least three times.WITH ConsecutiveNums AS (
+    SELECT
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+        LEAD(num, 2) OVER (ORDER BY id) AS next_next_num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LAG(num, 2) OVER (ORDER BY id) AS prev_prev_num
+    FROM Logs
+)
+SELECT DISTINCT
+    num AS ConsecutiveNums
+FROM ConsecutiveNums
+WHERE (num = next_num AND num = next_next_num)
+   OR (num = prev_num AND num = next_num)
+   OR (num = prev_prev_num AND num = prev_num);
