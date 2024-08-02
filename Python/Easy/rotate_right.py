dda28ba394af5a4c054e73f4fa720addc1cd64ea
naresy <nareshupadhayay74@gmail.com>
2024-08-01 23:40:01 -0700

    new_solutions

--- Python/Easy/rotate_right.py
+++ Python/Easy/rotate_right.py
@@ -11,3 +11,21 @@ print(result1)  # Output: [5, 6, 7, 1, 2, 3, 4]
 nums2 = [-1, -100, 3, 99]
 result2 = rotate(nums2, 2)
 print(result2) 
+
+# another way to solve the question
+class Solution(object):
+    def rotate(self, nums, k):
+
+        n = len(nums)
+        k = k % n  
+        temp = [0] * n  
+    
+        for i in range(n):
+
+            temp[(i + k) % n] = nums[i]
+        for i in range(n):
+
+           nums[i] = temp[i]
+    
+        return nums
+        
