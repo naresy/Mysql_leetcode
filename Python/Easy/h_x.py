20ca4dc76e78a0a45286af2557b623679d005aac
naresy <nareshupadhayay74@gmail.com>
2024-08-05 23:46:51 -0700

    solution

--- Python/Easy/h_x.py
+++ Python/Easy/h_x.py
@@ -14,3 +14,14 @@
 
 # Input: citations = [1,3,1]
 # Output: 1
+
+class Solution(object):
+    def hIndex(self, citations):
+        citations.sort(reverse=True)
+        h = 0
+        for i in range(len(citations)):
+            if citations[i] >= i + 1:
+                h = i + 1
+            else:
+                break
+        return h
