022428611a61f383b495815b95f411c846c48e57
naresy <nareshupadhayay74@gmail.com>
2024-08-06 23:15:16 -0700

    problem

---
+++ SQL/Medium/ Consecutive_Numbers.sql
@@ -0,0 +1,17 @@
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column.
+ 
+
+-- Find all numbers that appear at least three times consecutively.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
