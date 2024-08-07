b98da3b84d5fe45ce72fa5ab54b134acdec71298
naresy <nareshupadhayay74@gmail.com>
2024-08-06 23:25:21 -0700

    solution

--- SQL/Medium/Product_Price_at_a_Given_Date.sql
+++ SQL/Medium/Product_Price_at_a_Given_Date.sql
@@ -41,3 +41,15 @@
 -- | 1          | 35    |
 -- | 3          | 10    |
 -- +------------+-------+1s 
+SELECT 
+    p.product_id, 
+    COALESCE(
+        (SELECT new_price 
+         FROM Products AS p2 
+         WHERE p2.product_id = p.product_id 
+           AND p2.change_date <= '2019-08-16' 
+         ORDER BY p2.change_date DESC 
+         LIMIT 1), 
+        10) AS price
+FROM 
+    (SELECT DISTINCT product_id FROM Products) AS p;
