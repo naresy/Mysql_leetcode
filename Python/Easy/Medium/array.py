cab86d9fd9d9a508c0540c9405cd305ef052ad5b
naresy <nareshupadhayay74@gmail.com>
2024-08-13 23:22:26 -0700

    solution

--- Python/Easy/Medium/array.py
+++ Python/Easy/Medium/array.py
@@ -15,3 +15,25 @@
 #             right_product *= nums[i]
     
 #         return answer
+
+class Solution(object):
+   def product_except_self(self, nums):
+        n = len(nums)
+        answer = [1] * n
+    
+        # Fill left products
+        left_product = 1
+        for i in range(n):
+            answer[i] = left_product
+            left_product *= nums[i]
+    
+        # Fill right products and calculate the result
+        right_product = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= right_product
+            right_product *= nums[i]
+    
+        return answer
+
+
+   
