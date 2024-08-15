9308a3031ad2f8eec78a00327f1e417906867b0b
naresy <nareshupadhayay74@gmail.com>
2024-08-14 22:17:54 -0700

    solution

--- Python/Easy/Medium/gas_station.py
+++ Python/Easy/Medium/gas_station.py
@@ -42,17 +42,18 @@ class Solution(object):
     def canCompleteCircuit(self, gas, cost):
         total_gas=0
         total_cost=0
-        current_gas=0
+        current_position=0
         start_position=0
         for i in range(len(gas)):
             total_gas+=gas[i]
             total_cost+=cost[i]
-            current_gas=cost[i]-gas[i]
-            if current_gas>0:
+            current_position += gas[i] - cost[i]
+            if current_position<0:
                 start_position =i+1
                 current_position=0
         if total_gas<total_cost:
             return -1
         else:
             return start_position        
+         
         
