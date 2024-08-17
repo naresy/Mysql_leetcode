1ff65694d947422b27effe53e5bc7acf9e368f02
naresy <nareshupadhayay74@gmail.com>
2024-08-16 22:55:28 -0700

    Refactor

--- SQL/Medium/ Consecutive_Numbers.sql
+++ SQL/Medium/ Consecutive_Numbers.sql
@@ -31,3 +31,10 @@ FROM ConsecutiveNums
 WHERE (num = next_num AND num = next_next_num)
    OR (num = prev_num AND num = next_num)
    OR (num = prev_prev_num AND num = prev_num);
+
+-- short method
+SELECT DISTINCT L1.num AS ConsecutiveNums
+FROM Logs L1
+JOIN Logs L2 ON L1.id = L2.id + 1
+JOIN Logs L3 ON L2.id = L3.id + 1
+WHERE L1.num = L2.num AND L2.num = L3.num;
