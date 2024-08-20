e6bf1598b40a2612df77bb1a4413f056db4cbae4
naresy <nareshupadhayay74@gmail.com>
2024-08-19 23:30:00 -0700

    edit

--- Python/Medium/insert_delete_getRandom.py
+++ Python/Medium/insert_delete_getRandom.py
@@ -26,6 +26,9 @@
 # randomizedSet.insert(2); // 2 was already in the set, so return false.
 # randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
 
+
+# solution
+
 class RandomizedSet(object):
 
     def __init__(self):
