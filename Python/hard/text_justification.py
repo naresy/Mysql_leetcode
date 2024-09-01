64b041f0d5daeae2e733a25d0005d3de06daaf00
naresy <nareshupadhayay74@gmail.com>
2024-08-31 23:21:06 -0700

    solution

--- Python/hard/text_justification.py
+++ Python/hard/text_justification.py
@@ -56,3 +56,25 @@
 # words[i].length <= maxWidth
 
 # solution
+
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+         result = []  # List to store the fully justified lines
+         current_line = []  # List to store words for the current line
+         current_length =0
+         for word in words:  
+            if current_length + len(word) + len(current_line) > maxWidth:
+                for i in range(maxWidth - current_length):
+                    current_line[i % (len(current_line) - 1 or 1)] += ' '
+
+            
+                result.append(''.join(current_line))
+           
+                current_line, current_length = [], 0
+            current_line.append(word)
+            current_length += len(word)
+         result.append(' '.join(current_line).ljust(maxWidth))
+         return result  
+       
+
+
