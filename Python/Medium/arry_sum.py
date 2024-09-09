e299e4fdb4e42299ee915695a77dfa19264b4433
naresy <nareshupadhayay74@gmail.com>
2024-09-08 23:29:12 -0700

    problem

--- Python/Medium/arry_sum.py
+++ Python/Medium/arry_sum.py
@@ -34,3 +34,16 @@
 # The tests are generated such that there is exactly one solution.
 
 # solution
+class Solution(object):
+    def twoSum(self,numbers, target):
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            if current_sum == target:
+                return [left + 1, right + 1]  # Return 1-based indices
+            elif current_sum < target:
+                left += 1
+            else:
+                 right -= 1
+
+        
