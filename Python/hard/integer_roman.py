420931d36a76a7dfbfe876d254cd12c5ae95f993
naresy <nareshupadhayay74@gmail.com>
2024-10-25 23:19:14 -0400

    solution

--- Python/hard/integer_roman.py
+++ Python/hard/integer_roman.py
@@ -13,3 +13,29 @@
 # If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
 # If the value starts with 4 or 9 use the subtractive form representing one symbol subtracted from the following symbol, for example, 4 is 1 (I) less than 5 (V): IV and 9 is 1 (I) less than 10 (X): IX. Only the following subtractive forms are used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
 # Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
+
+def int_to_roman(num):
+    # Define the mapping of decimal values to Roman numeral symbols, in descending order
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    symbols = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    
+    roman_numeral = ""
+    
+    # Loop through each value-symbol pair
+    for i in range(len(val)):
+        # Append the symbol while subtracting its value from num
+        while num >= val[i]:
+            roman_numeral += symbols[i]
+            num -= val[i]
+    
+    return roman_numeral
